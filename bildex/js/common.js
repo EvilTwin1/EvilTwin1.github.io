f1550390bb9231c26b206872e6c5831eedc1fc1a
Vlad Zubkovskiy <izub1995@ukr.net>
2018-07-18 16:25:59 +0300

    bildex

---
+++ bildex/js/common.js
@@ -0,0 +1,59 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    dots:false,
+    autoHeight: true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$( document ).ready(function() {
+    $('#proj1').click(function(e){
+        e.preventDefault();
+        $("#info1").toggle();
+    });
+    
+    $('#proj2').click(function(e){
+        e.preventDefault();
+        $("#info2").toggle();
+    });
+    
+    $('#proj3').click(function(e){
+        e.preventDefault();
+        $("#info3").toggle();
+    });
+    
+    
+    
+
+    
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > $('.header').offset().top + 700) {
+            $(".menu-fix").css("display", "block");
+        }
+        else{
+            $(".menu-fix").css("display", "none");
+        }
+        
+    });
+    
+    
+    $('.arrow-down').on('click', function(){ 
+	var scroll_el = $(this).attr('href'); 
+        if ($('.about').length != 0) { 
+	       $('html, body').animate({ scrollTop: $('.about').offset().top }, 500); 
+        }
+    return false;
+});  
+    
+});
